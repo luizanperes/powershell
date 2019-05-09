a4a79dc9381bec47af8c0c1ec214224c39f0e6ca
Luiza Nascimento Peres <luizanperes@gmail.com>
2019-05-09 13:12:35 +0100

    Primeiro script em python

---
+++ python/soma.py
@@ -0,0 +1,20 @@
+a=1
+b=1
+soma=a+b
+print ("A soma é:",soma)
+
+#estrutura condicional IF
+if soma > 2:
+ print ("Essa soma é mais de 2")
+ 
+else:
+ print ("Essa soma é igual ou menor a 2")
+ 
+#array de 20 unidades 
+grupo=range(20)
+
+#laço de repetição for
+for elemento in grupo:
+ print ("TESTE:",elemento)
+ 
+#leia e veja videos sobre indentação; sem isso o codigo dá erros estranhos e dificeis de corrigir.
